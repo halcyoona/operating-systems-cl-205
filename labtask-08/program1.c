5b7e1f71482737633f030f31aca40d963c54d21b
halcyoona <halcyoona@gmail.com>
2019-03-29 12:28:29 +0500

    1

---
+++ labtask-08/program1.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+int main()
+{
+	int pid;
+	// for storing fork() return
+	int pfd[2];
+	// for pipe file descriptors
+	char aString[20];
+	// Temporary storage
+	pipe(pfd);
+	// create our pipe
+	pid = fork();
+	// create child process
+	if (pid == 0)
+	// For child
+	{
+		write(pfd[1], "Hello", 5);
+		// Write onto pipe
+		close(pfd[0]);
+	}
+	else // For parent
+	{
+		read(pfd[0], aString, 5);
+		// Read from pipe
+		close (pfd[1]);
+	}
+}
